32e05b9c60190b0c14c199dd057d465cbf0f7802
chagansingh <chagansingh9876@gmail.com>
2023-03-27 12:53:40 +0530

    requst send with body parser.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-
-
 const express = require('express')
+const bodyParser = require('body-parser');
+
 
 const app =express()
 
-app.use((req,res,next)=>{
-    console.log('in the middleware...')
-    next()
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><br/><input type="number" name="size"/><button type="submit">SUBMIT</button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log('in the next middleware...')
-    res.send('{ key1: value }')
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('hi bro your home')
 })
 
 app.listen(3000)
